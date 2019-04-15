106530ca605bdf2e325c9baf97f8e32282dfd792
tobiasgronne <tobias.groone@gmail.com>
2019-04-15 20:32:28 +0200

    Update umission.cpp

--- umission.cpp
+++ umission.cpp
@@ -167,7 +167,7 @@ int state = 0;
 bool ended = false;
 bool inManuel = false;
 int loop = 0;
-int missionState = 1;
+int missionState = 0;
 // initialize robot mission to do nothing (wait for mission lines)
 missionInit();
 bot->send("start\n"); // ask robot to start controlled run (ready to execute)
