911880cae8ab7107cbd5906d235cca5b2a0d114f
tobiasgronne <tobias.groone@gmail.com>
2019-04-22 20:21:58 +0200

    Update umission.cpp

--- umission.cpp
+++ umission.cpp
@@ -1004,7 +1004,7 @@ switch (state)
     snprintf(lines[1], MAX_LEN, "vel=0.0, acc=100.0, edgel=0.0, white=1:time=0.2");
     snprintf(lines[2], MAX_LEN, "tr=0.05,vel=0.3,acc=3:turn=-24");
 	snprintf(lines[3], MAX_LEN, "vel=-0.1,acc=3:dist=0.05");
-	snprintf(lines[4], MAX_LEN, "vel=0.0, acc=100.0, edgel=0.0, white=1:time=0.2");
+	snprintf(lines[4], MAX_LEN, "vel=0.0, acc=100.0:time=0.2");
 	snprintf(lines[5], MAX_LEN, "event=1:time=1.1");
     missionSendAndRun(lineList, 6);
     state++;
@@ -1034,11 +1034,11 @@ switch (state)
     snprintf(lines[0], MAX_LEN, "vel=0.4,acc=4,edger=0.0,white=1:dist=0.3");
                 snprintf(lines[1], MAX_LEN, "vel=1.0,acc=6,edgel=0.0,white=1:dist=0.3");
                 snprintf(lines[2], MAX_LEN, "vel=2.0,acc=3,edgel=0.0,white=1:dist=6.9");
-                snprintf(lines[3], MAX_LEN, "vel=0,acc=1000:time=1");
+                snprintf(lines[3], MAX_LEN, "vel=0,acc=10:time=1");
 				snprintf(lines[4], MAX_LEN, "vel=0.3,acc=2,edgel=0.0,white=1:xl>16");
 				snprintf(lines[5], MAX_LEN, "vel=0,acc=1000:time=1");
-    snprintf(lines[7], MAX_LEN, "event=1:time=1.1");
-    missionSendAndRun(lineList, 8);
+    snprintf(lines[6], MAX_LEN, "event=1:time=1.1");
+    missionSendAndRun(lineList, 7);
     state++;
     break;
         case 1:
