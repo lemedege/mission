ce14d82593eb42a4eee75eb9419b1a9cdf206096
tobiasgronne <tobias.groone@gmail.com>
2019-04-22 20:04:25 +0200

    Update umission.cpp

--- umission.cpp
+++ umission.cpp
@@ -719,7 +719,7 @@ bool UMission::trappetoreg(int & state) // from trappe end to regbot (aka center
         snprintf(lines[4], MAX_LEN, "vel=0.0, acc=100.0, edgel=0.0, white=1:time=0.1");
 		snprintf(lines[5], MAX_LEN, "tr=0.1,vel=0.2,acc=2:turn=90");
 		snprintf(lines[6], MAX_LEN, "vel=0.0,acc=5:time=0.1");
-		snprintf(lines[7], MAX_LEN, "vel=0.5,acc=5:dist=0.55");
+		snprintf(lines[7], MAX_LEN, "vel=0.5,acc=5:dist=0.51");
 		snprintf(lines[8], MAX_LEN, "vel=0.0, acc=100.0:time=0.1");
         snprintf(lines[9], MAX_LEN, "event=1:time=1.1");
 				missionSendAndRun(lineList, 10);
@@ -1003,7 +1003,7 @@ switch (state)
     snprintf(lines[0], MAX_LEN, "vel=0.3,acc=3,edger=0.0,white=1:dist=2.28");
     snprintf(lines[1], MAX_LEN, "vel=0.0, acc=100.0, edgel=0.0, white=1:time=0.2");
     snprintf(lines[2], MAX_LEN, "tr=0.05,vel=0.3,acc=3:turn=-24");
-	snprintf(lines[3], MAX_LEN, "vel=-0.1,acc=3:dist=0.15");
+	snprintf(lines[3], MAX_LEN, "vel=-0.1,acc=3:dist=0.5");
 	snprintf(lines[4], MAX_LEN, "vel=0.0, acc=100.0, edgel=0.0, white=1:time=0.2");
 	snprintf(lines[5], MAX_LEN, "event=1:time=1.1");
     missionSendAndRun(lineList, 6);
@@ -1031,7 +1031,7 @@ bool finished = false;
 switch (state)
 {
     case 0: //race track 
-    snprintf(lines[0], MAX_LEN, "vel=0.4,acc=4,edger=2.0,white=1:dist=0.2");
+    snprintf(lines[0], MAX_LEN, "vel=0.4,acc=2,edger=0.0,white=1:dist=0.2");
                 snprintf(lines[1], MAX_LEN, "vel=1.0,acc=4,edgel=0.0,white=1:dist=0.2");
                 snprintf(lines[2], MAX_LEN, "vel=2.0,acc=8,edgel=0.0,white=1:xl>8");
                 snprintf(lines[3], MAX_LEN, "vel=2.0,acc=5,edgel=0.0,white=1:dist=3");
