0a0efdbd86f527a92dbe899dd9c2381c3049f0ef
tobiasgronne <tobias.groone@gmail.com>
2019-04-24 11:51:37 +0200

    Update umission.cpp

--- umission.cpp
+++ umission.cpp
@@ -636,7 +636,7 @@ bool UMission::trappedims(int & state) // gullutine port -> ramp up, tippe ned r
 		snprintf(lines[0], MAX_LEN, "tr=0.0,vel=0.2,acc=1:turn=-90");
 		snprintf(lines[1], MAX_LEN, "vel=0.5,acc=5,edger=1.0:dist=2.60");
 		snprintf(lines[2], MAX_LEN, "vel=0,acc=100:time=0.2");
-		snprintf(lines[4], MAX_LEN, "vel=0.2,acc=2:time=2,dist=0.11");
+		snprintf(lines[4], MAX_LEN, "vel=0.2,acc=2:time=2,dist=0.6");
 		snprintf(lines[5], MAX_LEN, "tr=0,vel=0.5,acc=2:turn=-90");
 		snprintf(lines[6], MAX_LEN, "vel=0,acc=100:time=0.4");
 		snprintf(lines[7], MAX_LEN, "vel=0.2,acc=2:time=2,dist=0.12");
@@ -958,11 +958,11 @@ bool UMission::tunel(int & state) // trough tunnel closing doors a
 	case 40: //Through Gate
 				snprintf(lines[0], MAX_LEN, "vel=0.3,acc=5,white=1:xl>18");
 				snprintf(lines[1], MAX_LEN, "tr=0.0,vel=0.3,acc=3:turn=-90");
-				snprintf(lines[2], MAX_LEN, "vel=0.1,acc=3,edgel=0.0,white=1:time=7.0");
+				snprintf(lines[2], MAX_LEN, "vel=0.25,acc=3,edgel=0.0,white=1:time=7.0");
 				snprintf(lines[3], MAX_LEN, "vel=-0.1,acc=3:dist=0.1");
 				snprintf(lines[4], MAX_LEN, "vel=-0.1,acc=3:dist=0.1");
                 snprintf(lines[5], MAX_LEN, "tr=0.0,vel=0.3,acc=3:turn=180");
-                snprintf(lines[6], MAX_LEN, "vel=-0.2,acc=30:time=4");
+                snprintf(lines[6], MAX_LEN, "vel=-0.35,acc=30:time=4");
 				snprintf(lines[7], MAX_LEN, "vel=0.0, acc=100.0, edgel=0.0, white=1:time=0.5");
                 snprintf(lines[8], MAX_LEN, "event=1:time=1.1");
 				missionSendAndRun(lineList, 9);
