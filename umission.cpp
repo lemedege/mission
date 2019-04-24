cf666f5dee7ac2203d496f60681cc824afe564fa
tobiasgronne <tobias.groone@gmail.com>
2019-04-24 11:54:48 +0200

    Update umission.cpp

--- umission.cpp
+++ umission.cpp
@@ -636,7 +636,7 @@ bool UMission::trappedims(int & state) // gullutine port -> ramp up, tippe ned r
 		snprintf(lines[0], MAX_LEN, "tr=0.0,vel=0.2,acc=1:turn=-90");
 		snprintf(lines[1], MAX_LEN, "vel=0.5,acc=5,edger=1.0:dist=2.60");
 		snprintf(lines[2], MAX_LEN, "vel=0,acc=100:time=0.2");
-		snprintf(lines[4], MAX_LEN, "vel=0.2,acc=2:time=2,dist=0.6");
+		snprintf(lines[4], MAX_LEN, "vel=0.2,acc=2:time=2,dist=0.06");
 		snprintf(lines[5], MAX_LEN, "tr=0,vel=0.5,acc=2:turn=-90");
 		snprintf(lines[6], MAX_LEN, "vel=0,acc=100:time=0.4");
 		snprintf(lines[7], MAX_LEN, "vel=0.2,acc=2:time=2,dist=0.12");
